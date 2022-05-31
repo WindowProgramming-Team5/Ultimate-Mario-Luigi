4c22a9e28371e9bd3e6f69add1bb41ccd1490bbf
syyuuun <02tjdduf@gmail.com>
2022-06-01 00:41:01 +0900

    캐릭터 구현

--- Main.cpp
+++ Main.cpp
@@ -15,9 +15,12 @@ using std::default_random_engine;
 using std::random_device;
 using std::uniform_int_distribution;
 
+enum class Dir{LEFT=0,RIGHT,UP,DOWN};
+
 struct Player{
 	RECT rect;
 	int aIndex; // Animation Index
+	Dir dir;
 };
 
 struct Monster {
@@ -29,14 +32,15 @@ struct Item {
 	RECT rect;
 };
 
-Player Mario;
-Player Luigi;
+Player mario;
+Player luigi;
 
-Monster Gumba[MAX_MONSTER];
-Monster Troopa[MAX_MONSTER];
+Monster gumba[MAX_MONSTER];
+Monster troopa[MAX_MONSTER];
 
 RECT gateSpot;	// EndLineBox
 
+RECT temp;	// Just temp
 HINSTANCE g_hInst;
 LPCTSTR lpszClass = L"Window class Name";
 LPCTSTR lpszWindowName = L"Window Programming Lab";
@@ -96,7 +100,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 	case WM_CREATE:
 		backGround = LoadBitmap(g_hInst, MAKEINTRESOURCE(IDB_BITMAP1));
-		//hGumba = LoadBitmap(g_hInst, MAKEINTRESOURCE(IDB_BITMAP2));
+		hMario = LoadBitmap(g_hInst, MAKEINTRESOURCE(IDB_BITMAP4));
+		hLuigi = LoadBitmap(g_hInst, MAKEINTRESOURCE(IDB_BITMAP5));
 		gate = LoadBitmap(g_hInst, MAKEINTRESOURCE(IDB_BITMAP3));
 
 		gateSpot.left = 1050;
@@ -104,41 +109,79 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		gateSpot.right = 1200;
 		gateSpot.bottom = 150;
 
-		Mario.rect.left = 100;
-		Mario.rect.top = 100;
-		Mario.rect.right = 100;
-		Mario.rect.bottom = 100;
+		/* Mario start position */
+		mario.rect.left = 100;
+		mario.rect.top = 100;
+		mario.rect.right = mario.rect.left+80;
+		mario.rect.bottom = mario.rect.top + 80;
 
-		Gumba[0].rect.left = 100;
-		Gumba[0].rect.top = 100;
-		Gumba[0].rect.right  = 200;
-		Gumba[0].rect.bottom = 200;
+		/* Luigi start position */
+		luigi.rect.left = 200;
+		luigi.rect.top = 200;
+		luigi.rect.right = luigi.rect.left+75;
+		luigi.rect.bottom = luigi.rect.top+80;
+		
+		SetTimer(hWnd, 1000, 5000, NULL);
 		break;
 	case WM_CHAR:
 		// W A S D -> Mario Move, I J K L -> Luigi Move
 		switch (tolower(wParam))
 		{
 		case 'w':
+			mario.rect.top -= 5;
+			mario.rect.bottom -= 5;
+			mario.dir = Dir::UP;
 			break;
 		case 's':
+			mario.rect.top += 5;
+			mario.rect.bottom += 5;
+			mario.dir = Dir::DOWN;
 			break;
 		case 'a':
+			mario.rect.left -= 5;
+			mario.rect.right -= 5;
+			mario.dir = Dir::LEFT;
 			break;
 		case 'd':
+			mario.rect.left += 5;
+			mario.rect.right += 5;
+			mario.dir = Dir::RIGHT;
 			break;
 		case 'i':
+			luigi.rect.top -= 5;
+			luigi.rect.bottom -= 5;
+			luigi.dir = Dir::UP;
 			break;
 		case 'k':
+			luigi.rect.top += 5;
+			luigi.rect.bottom += 5;
+			luigi.dir = Dir::DOWN;
 			break;
 		case 'j':
+			luigi.rect.left -= 5;
+			luigi.rect.right -= 5;
+			luigi.dir = Dir::LEFT;
 			break;
 		case 'l':
+			luigi.rect.left += 5;
+			luigi.rect.right += 5;
+			luigi.dir = Dir::DOWN;
 			break;
+
 		case 'g':	// Into Gate
+			// Mario into gate
+			if (IntersectRect(&temp, &gateSpot, &mario.rect)) {
+
+			}
+			// Luigi into gate
+			else if (IntersectRect(&temp, &gateSpot, &luigi.rect)) {
+
+			}
 			break;
 		default:
 			break;
 		}
+		InvalidateRect(hWnd, NULL, FALSE);
 		break;
 	case WM_PAINT:
 		hDC = BeginPaint(hWnd, &ps);
@@ -151,12 +194,23 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
 		DrawBackGround(backMemDC, memDC, backGround,oldBackGround);
 		DrawGate(backMemDC, memDC, gate, oldGate);
-		//DrawMario(backMemDC, memDC, hMario);
-		//DrawLuigi(backMemDC, memDC, hLuigi);
+		DrawMario(backMemDC, memDC, hMario,oldMario);
+		DrawLuigi(backMemDC, memDC, hLuigi,oldLuigi);
+
 
 		//DrawGumba(backMemDC, memDC, hGumba);
 		BitBlt(hDC, 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, backMemDC, 0, 0, SRCCOPY);
 		EndPaint(hWnd, &ps);
+		break;
+	case WM_TIMER:
+		switch (wParam)
+		{
+			/* Monster Spawning */
+		case 1000:
+			break;
+		default:
+			break;
+		}
 		break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
@@ -166,7 +220,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	}
 	return DefWindowProc(hWnd, uMsg, wParam, lParam);
 }
-
 
 void DrawBackGround(HDC backMemDC, HDC memDC, HBITMAP backGround, HBITMAP oldBackGround)
 {
@@ -181,19 +234,23 @@ void DrawGate(HDC backMemDC, HDC memDC, HBITMAP gate, HBITMAP oldGate)
 	TransparentBlt(backMemDC, gateSpot.left-10, gateSpot.top, 130, 130, memDC, 355, 30, 150, 150, RGB(255, 0, 255));
 	SelectObject(memDC, oldGate);
 }
+
 void DrawMario(HDC backMemDC, HDC memDC, HBITMAP hMario, HBITMAP oldMario)
 {
-	SelectObject(memDC, hMario);
-	TransparentBlt(backMemDC, 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, memDC, 2, 11, 510, 510, RGB(255, 0, 0));
+	oldMario = static_cast<HBITMAP>(SelectObject(memDC, hMario));
+	TransparentBlt(backMemDC, mario.rect.left, mario.rect.top, 80, 80, memDC, 12, 251, 34, 38, RGB(0, 174, 0));
+	SelectObject(memDC, oldMario);
 }
 
 void DrawLuigi(HDC backMemDC, HDC memDC, HBITMAP hLuigi, HBITMAP oldLuigi)
 {
-
+	oldLuigi = static_cast<HBITMAP>(SelectObject(memDC, hLuigi));
+	TransparentBlt(backMemDC, luigi.rect.left, luigi.rect.top, 75, 80, memDC, 4, 256, 24, 44, RGB(0, 174, 174));
+	SelectObject(memDC, oldLuigi);
 }
 
-void DrawGumba(HDC backMemDC, HDC memDC, HBITMAP hGumba, HBITMAP oldGumba)
-{
-	SelectObject(memDC, hGumba);
-	TransparentBlt(backMemDC, Gumba[0].rect.left, Gumba[0].rect.top, 40, 40, memDC, 0, 145, 27, 23, RGB(0, 162, 232));
-}
+//void DrawGumba(HDC backMemDC, HDC memDC, HBITMAP hGumba, HBITMAP oldGumba)
+//{
+//	SelectObject(memDC, hGumba);
+//	TransparentBlt(backMemDC, Gumba[0].rect.left, Gumba[0].rect.top, 40, 40, memDC, 0, 145, 27, 23, RGB(0, 162, 232));
+//}
